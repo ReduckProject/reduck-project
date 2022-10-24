de7a44d69fda0adef111c10c958095c5df9e55fd
Reduck <838288125@qq.com>
2022-10-24 17:07:00 +0800

    module # reduck-jasypt

---
+++ reduck-jasypt/src/main/java/net/reduck/jasypt/ReduckJasyptApplication.java
@@ -0,0 +1,13 @@
+package net.reduck.jasypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReduckJasyptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReduckJasyptApplication.class, args);
+    }
+
+}
---
+++ reduck-jasypt/src/test/java/net/reduck/jasypt/ReduckJasyptApplicationTests.java
@@ -0,0 +1,13 @@
+package net.reduck.jasypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReduckJasyptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
