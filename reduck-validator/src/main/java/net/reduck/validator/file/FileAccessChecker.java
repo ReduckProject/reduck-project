c8df7ce400f535951c047fb4606ca4c0aec94128
Reduck <838288125@qq.com>
2023-01-30 10:21:52 +0800

    [validator] feat # add FileAccessChecker, which can help is accessed the file

---
+++ reduck-validator/src/main/java/net/reduck/validator/file/FileAccessChecker.java
@@ -0,0 +1,47 @@
+package net.reduck.validator.file;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Gin
+ * @since 2023/1/30 09:54
+ */
+public class FileAccessChecker {
+    private final String canonicalParentPath;
+
+    public FileAccessChecker(String parentPath) {
+        try {
+            File file = new File(parentPath);
+
+            if(!file.isDirectory()) {
+                throw new RuntimeException("File must be directory");
+            }
+            this.canonicalParentPath = file.getCanonicalPath() + File.separatorChar;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public FileAccessChecker(File parentFile) {
+        try {
+            this.canonicalParentPath = parentFile.getCanonicalPath();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean access(String filePath) {
+        return access(new File(filePath));
+    }
+
+    public boolean access(File file) {
+        String canonicalPath = null;
+        try {
+            canonicalPath = file.getCanonicalPath();
+        } catch (IOException e) {
+            return false;
+        }
+        return canonicalPath.contains(canonicalParentPath);
+    }
+}
