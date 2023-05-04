8ae8221dc710fcc066339349303e74902fa18596
Reduck <838288125@qq.com>
2023-05-04 18:08:19 +0800

    [leetcode] module

---
+++ reduck-leetcode/src/main/java/net/reduck/leetcode/ReduckLeetcodeApplication.java
@@ -0,0 +1,13 @@
+package net.reduck.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReduckLeetcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReduckLeetcodeApplication.class, args);
+    }
+
+}
