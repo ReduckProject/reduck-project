4b575c0140c3ec56339d4c472ca72246acaaac99
Reduck <838288125@qq.com>
2023-05-06 10:18:44 +0800

    [apt] fix # fix generate annotation error

---
+++ reduck-processor/src/main/java/net/reduck/apt/processor/AstMojo.java
@@ -0,0 +1,55 @@
+package net.reduck.apt.processor;
+
+import com.sun.tools.javac.api.JavacTrees;
+import com.sun.tools.javac.code.Symtab;
+import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.TreeMaker;
+import com.sun.tools.javac.util.Name;
+import com.sun.tools.javac.util.Names;
+
+import javax.lang.model.element.Element;
+
+/**
+ * @author Gin
+ * @since 2023/5/6 09:58
+ */
+public class AstMojo {
+
+    private final JavacTrees trees;
+
+    private final TreeMaker treeMaker;
+
+    private final Names names;
+
+    private final Symtab symtab;
+
+    public AstMojo(JavacTrees trees, TreeMaker treeMaker, Names names, Symtab symtab) {
+        this.trees = trees;
+        this.treeMaker = treeMaker;
+        this.names = names;
+        this.symtab = symtab;
+    }
+
+    public void importIfAbsent(Element element, Class<?> importType) {
+        JCTree.JCCompilationUnit compilationUnit = ((JCTree.JCCompilationUnit) trees.getPath(element).getCompilationUnit());
+        boolean contains = false;
+        for (JCTree.JCImport jcImport : compilationUnit.getImports()) {
+            if (importType.getName().equals(jcImport.getQualifiedIdentifier().toString())) {
+                contains = true;
+                break;
+            }
+        }
+
+        if(!contains) {
+            JCTree.JCIdent jcIdent = treeMaker.Ident(names.fromString(importType.getPackage().getName()));
+            Name className = names.fromString(importType.getSimpleName());
+            JCTree.JCFieldAccess jcFieldAccess = treeMaker.Select(jcIdent, className);
+            JCTree.JCImport jcImport = treeMaker.Import(jcFieldAccess, false);
+            compilationUnit.defs = compilationUnit.defs.prepend(jcImport);
+        }
+    }
+
+    public JCTree.JCIdent identFromString(String name) {
+        return treeMaker.Ident(names.fromString(name));
+    }
+}
--- reduck-processor/src/main/java/net/reduck/apt/processor/CryptoProcessor.java
+++ reduck-processor/src/main/java/net/reduck/apt/processor/CryptoProcessor.java
@@ -1,14 +1,9 @@
 package net.reduck.apt.processor;
 
-import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.RoundEnvironment;
-import javax.lang.model.element.TypeElement;
-import java.util.Set;
-
 import com.sun.source.util.TreePath;
 import com.sun.tools.javac.api.JavacTrees;
+import com.sun.tools.javac.code.Symbol;
 import com.sun.tools.javac.code.Symtab;
-import com.sun.tools.javac.code.TypeTag;
 import com.sun.tools.javac.processing.JavacProcessingEnvironment;
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.TreeMaker;
@@ -58,16 +53,20 @@ public class CryptoProcessor extends AbstractProcessor {
 
     private Symtab symtab;
 
+    private AstMojo astMojo;
+
     FileOutputStream os = new FileOutputStream(new File(System.getProperty("user.home") + "/Downloads/processor.log"), true);
 
-    private String convertPackageName = "javax.persistence";
-    private String convertName = "Convert";
+    private String convertPackageName = CryptoConverter.class.getPackage().getName();
+    private String convertName = CryptoConverter.class.getSimpleName();
 
     private Class convertClass = Crypto.class;
-    private String convertFiledPackageName = CryptoConverter.class.getPackage().getName();
+    private String convertFiledPackageName = Test.class.getPackage().getName();
     private String convertFiled = Test.class.getSimpleName();
 
     private final String cryptoFullName = Crypto.class.getName();
+
+    private JCTree.JCAnnotation templateAnnotation;
 
     public CryptoProcessor() throws FileNotFoundException {
     }
@@ -82,29 +81,31 @@ public class CryptoProcessor extends AbstractProcessor {
             TypeElement typeElem = (TypeElement) element.getEnclosingElement();
 
             String typeName = typeElem.getQualifiedName().toString();
-
-            if (!existPackage.containsKey(typeName)) {
-                JCTree.JCIdent jcIdent = treeMaker.Ident(names.fromString(convertPackageName));
-                Name className = names.fromString(convertName);
-                JCTree.JCFieldAccess jcFieldAccess = treeMaker.Select(jcIdent, className);
-                JCTree.JCImport anImport = treeMaker.Import(jcFieldAccess, false);
-
-                // 导入注解类
-                TreePath path = trees.getPath(typeElem);
-                JCTree.JCCompilationUnit jccu = (JCTree.JCCompilationUnit) path.getCompilationUnit();
-                jccu.defs = jccu.defs.prepend(anImport);
-
-                JCTree.JCIdent jcIdent2 = treeMaker.Ident(names.fromString(convertFiledPackageName));
-                Name className2 = names.fromString(convertFiled);
-                JCTree.JCFieldAccess jcFieldAccess2 = treeMaker.Select(jcIdent2, className2);
-                JCTree.JCImport anImport2 = treeMaker.Import(jcFieldAccess2, false);
-                // 导入转化类
-                TreePath path2 = trees.getPath(typeElem);
-                JCTree.JCCompilationUnit jccu2 = (JCTree.JCCompilationUnit) path2.getCompilationUnit();
-                jccu2.defs = jccu2.defs.prepend(anImport2);
-
-                existPackage.put(typeName, "Exist");
-            }
+            astMojo.importIfAbsent(element, CryptoConverter.class);
+            astMojo.importIfAbsent(element, Test.class);
+//            if (!existPackage.containsKey(typeName)) {
+//                TreePath path = trees.getPath(typeElem);
+//                JCTree.JCIdent jcIdent = treeMaker.Ident(names.fromString(convertPackageName));
+//                Name className = names.fromString(convertName);
+//                JCTree.JCFieldAccess jcFieldAccess = treeMaker.Select(jcIdent, className);
+//                JCTree.JCImport anImport = treeMaker.Import(jcFieldAccess, false);
+//
+//                // 导入注解类
+//                TreePath path = trees.getPath(typeElem);
+//                JCTree.JCCompilationUnit jccu = (JCTree.JCCompilationUnit) path.getCompilationUnit();
+//                jccu.defs = jccu.defs.prepend(anImport);
+//
+//                JCTree.JCIdent jcIdent2 = treeMaker.Ident(names.fromString(convertFiledPackageName));
+//                Name className2 = names.fromString(convertFiled);
+//                JCTree.JCFieldAccess jcFieldAccess2 = treeMaker.Select(jcIdent2, className2);
+//                JCTree.JCImport anImport2 = treeMaker.Import(jcFieldAccess2, false);
+//                // 导入转化类
+//                TreePath path2 = trees.getPath(typeElem);
+//                JCTree.JCCompilationUnit jccu2 = (JCTree.JCCompilationUnit) path2.getCompilationUnit();
+//                jccu2.defs = jccu2.defs.prepend(anImport2);
+//
+//                existPackage.put(typeName, "Exist");
+//            }
 
             JCTree jcTree = trees.getTree(typeElem);
 
@@ -123,18 +124,60 @@ public class CryptoProcessor extends AbstractProcessor {
                                 if (Crypto.class.getName().equals(jcAnnotation.getAnnotationType().type.tsym.toString())) {
                                     log("append: ...");
                                     JCTree.JCAnnotation converterAnnotation = treeMaker.Annotation(
-                                            treeMaker.Ident(names.fromString(CryptoConverter.class.getSimpleName())),
-                                            List.of(
-                                                    // convert() 默认为 Void.class
-                                                    treeMaker.Assign(
-                                                            treeMaker.Ident(names.fromString("convert")),
-                                                            treeMaker.Ident(names.fromString(Test.class.getSimpleName()))
-                                                    )
-                                            )
+                                            select(CryptoConverter.class.getName()),
+//                                            List.of(
+//                                                    // convert() 默认为 Void.class
+//                                                    treeMaker.Assign(
+//                                                            treeMaker.Ident(names.fromString("convert")),
+//                                                            select(Test.class.getName()))
+//                                                    )
+                                            List.nil()
                                     );
+
+                                    converterAnnotation = treeMaker.Annotation(
+                                            select(CryptoConverter.class.getName())
+                                            ,List.of(treeMaker.Assign(treeMaker.Ident(names.fromString("convert"))
+                                                            , treeMaker.Select(treeMaker.Ident(names.fromString(Test.class.getSimpleName()))
+                                                            , names.fromString("class")))));
+                                    // 构造注解参数
+//                                    JCTree.JCExpression arg = treeMaker.Ident(names.fromString("Test.class"));
+//                                    List<JCTree.JCExpression> args = List.of(arg);// 构造注解
+//                                    JCTree.JCAnnotation annotation = treeMaker.Annotation(
+//                                            treeMaker.Ident(names.fromString("CryptoConverter.class")),
+//                                            args
+//                                    );
+
                                     nil = nil.append(converterAnnotation);
+                                    if (templateAnnotation != null) {
+//                                        nil = nil.append(templateAnnotation);
+                                        log(templateAnnotation.getTag()
+                                                + "," + templateAnnotation.getKind()
+                                                +  "," + templateAnnotation.getAnnotationType()
+                                                +  "," + templateAnnotation.getStartPosition()
+                                                +  "," + templateAnnotation.getPreferredPosition()
+                                                +  "," + templateAnnotation.getArguments()
+                                                +  "," + templateAnnotation.getTree()
+                                                +  "," + templateAnnotation.getTree().type
+                                                +  "," + templateAnnotation.getTree().pos
+                                        );
+
+//                                        converterAnnotation = annotation;
+                                        log(converterAnnotation.getTag()
+                                                + "," + converterAnnotation.getKind()
+                                                +  "," + converterAnnotation.getAnnotationType()
+                                                +  "," + converterAnnotation.getStartPosition()
+                                                +  "," + converterAnnotation.getPreferredPosition()
+                                                +  "," + converterAnnotation.getArguments()
+                                                +  "," + converterAnnotation.getTree()
+                                                +  "," + converterAnnotation.getTree().type
+                                                +  "," + converterAnnotation.getTree().pos
+                                        );
+                                    }
                                     log("append:" + converterAnnotation.toString());
                                 } else {
+                                    if (CryptoConverter.class.getName().equals(jcAnnotation.getAnnotationType().type.tsym.toString())) {
+                                        templateAnnotation = jcAnnotation;
+                                    }
                                     nil = nil.append(jcAnnotation);
                                     log("append original:" + jcAnnotation.toString());
                                 }
@@ -153,7 +196,7 @@ public class CryptoProcessor extends AbstractProcessor {
 
                     try {
                         super.visitVarDef(jcVariableDecl);
-                    }catch (Exception e) {
+                    } catch (Exception e) {
                         log("error :" + e.getMessage());
                         log("error :" + e.toString());
                     }
@@ -163,6 +206,36 @@ public class CryptoProcessor extends AbstractProcessor {
         }
 
         return true;
+    }
+
+    JCTree.JCExpression select(String path) {
+        JCTree.JCExpression expression = null;
+        int i = 0;
+        for (String split : path.split("\\.")) {
+            if (i == 0)
+                expression = treeMaker.Ident(names.fromString(split));
+            else {
+                expression = treeMaker.Select(expression, names.fromString(split));
+            }
+            i++;
+        }
+
+        return expression;
+    }
+
+    JCTree.JCExpression select2(String path) {
+        JCTree.JCExpression expression = null;
+        int i = 0;
+        for (String split : path.split("\\.")) {
+            if (i == 0)
+                expression = treeMaker.Ident(names.fromString(split));
+            else {
+                expression = treeMaker.Select(expression, names.fromString("class"));
+            }
+            i++;
+        }
+
+        return expression;
     }
 
     @Override
@@ -179,6 +252,8 @@ public class CryptoProcessor extends AbstractProcessor {
         this.treeMaker = TreeMaker.instance(context);
         this.names = Names.instance(context);
         this.symtab = Symtab.instance(context);
+
+        this.astMojo = new AstMojo(trees, treeMaker, names, symtab);
     }
 
     private void log(String message) {
